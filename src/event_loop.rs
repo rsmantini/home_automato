d6a3f3d5fd0efa3db09d4527d2d34d2782517434
Rodrigo Mantini <rodrigo.mantini@gmail.com>
2022-01-25 22:56:05 +0100

    block on request_processor until next execution time

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -2,7 +2,7 @@ use super::components::*;
 use super::lcn_config;
 use super::requests::*;
 use super::systems;
-use rocket::tokio::sync::mpsc::UnboundedReceiver;
+use rocket::tokio::{runtime::Runtime, sync::mpsc::UnboundedReceiver};
 
 pub fn run(mut rx: UnboundedReceiver<Request>) -> Result<(), String> {
     let mut world = lame_ecs::create_world!();
@@ -11,10 +11,9 @@ pub fn run(mut rx: UnboundedReceiver<Request>) -> Result<(), String> {
         .build()
         .expect("could not init http client");
     let lcn_config = lcn_config::from_file("lcn_config.json").expect("could not parse config file");
-
+    let runtime = Runtime::new().expect("could not create tokio runtime");
     loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        systems::request_processor::process(&mut world, &mut rx)?;
+        runtime.block_on(systems::request_processor::process(&mut world, &mut rx))?;
         systems::scheduler::process(&mut world);
         systems::lcn_command_executor::process(&mut world, &lcn_config, &client);
     }
